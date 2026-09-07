9860c507ac14ddf7f35dc39fddfa3424e25b32cb
MAC fleet <mac-fleet@nvidia.com>
2026-09-07 02:21:40 -0700

    auto-commit: task_07becd2755904fdd0ce0cb1f6f2ed4ed

--- toolkit/__init__.py
+++ toolkit/__init__.py
@@ -7,8 +7,20 @@ keep this file additive-only, one line per merged module.
 
 from toolkit.bloom_filter import BloomFilter as BloomFilter
 from toolkit.graph_topo_sort import topological_sort as topological_sort
+from toolkit.lexer import LexerError as LexerError
+from toolkit.lexer import Token as Token
+from toolkit.lexer import TokenType as TokenType
+from toolkit.lexer import tokenize as tokenize
 from toolkit.lru_cache import LRUCache as LRUCache
 from toolkit.union_find import UnionFind as UnionFind
-from toolkit.graph_topo_sort import topological_sort as topological_sort
 
-__all__: list[str] = ["BloomFilter", "LRUCache", "UnionFind", "topological_sort"]
+__all__: list[str] = [
+    "BloomFilter",
+    "LexerError",
+    "LRUCache",
+    "Token",
+    "TokenType",
+    "UnionFind",
+    "topological_sort",
+    "tokenize",
+]
